ea3c36bc042705dcd051b143fdb48ae8e922a835
Gaetano Osur <165330888+osur-gaetano@users.noreply.github.com>
2024-07-06 10:26:48 +0300

    Add User model
    The User model has been added through devise gem, to handle authenticaion
    and authorization.
    
    Modified the application_controller to allow :username as parameter
    in the sign up process.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 require "application_responder"
 
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   self.responder = ApplicationResponder
   respond_to :html
 
+  def configure_permitted_parameters
+    devise_parameter_sanitezer.permit(:sign_up, keys: [:username])
 end
